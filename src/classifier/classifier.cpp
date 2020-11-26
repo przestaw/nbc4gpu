41ff58f65e051d08984f99aeb27091e456d959b8
przestaw <przemek.stawczyk@gmail.com>
2020-11-26 15:37:42 +0100

    dummy doxygen example

---
+++ src/classifier/classifier.cpp
@@ -0,0 +1,17 @@
+//
+// Created by przestaw on 20.10.2020.
+//
+#include <project_defines.h>
+DIAGNOSTIC_PUSH
+#include <boost/compute.hpp>
+DIAGNOSTIC_POP
+#include <classifier/classifier.h>
+#include <error/exeception.h>
+
+using namespace boost::compute;
+
+nbc4gpu::Classifier::Classifier() : field(0) {}
+
+double nbc4gpu::Classifier::predict(const nbc4gpu::Classifier::Record& record) {
+  throw nbc4gpu::error::runtime_exception("Not implemented");
+}
--- src/dummy/dummy.cpp
+++
@@ -1,8 +0,0 @@
-//
-// Created by przestaw on 20.10.2020.
-//
-#include <project_defines.h>
-DIAGNOSTIC_PUSH
-#include <boost/compute.hpp>
-DIAGNOSTIC_POP
-using namespace boost::compute;
